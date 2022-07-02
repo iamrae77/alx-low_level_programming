4dbee09a612e466e1abd434be9bc02b2cf62f34b
iamrae77 <beccapeters007@gmail.com>
2022-07-01 18:40:44 -0700

    Modified 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,11 +6,12 @@
 * Main - Entry point
 * This program checks if the value stored in the variable
 * 'n' is positive or negative
-* Return 0 (Success)
+* Return: 0 (Success)
 */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
