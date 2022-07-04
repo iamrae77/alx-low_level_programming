a088c7ef1d0aae7625cd5e7e22a55db12a520d60
iamrae77 <beccapeters007@gmail.com>
2022-07-03 19:27:46 -0700

    Revert "UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity"
    This reverts commit d9465aac2d7fab3a67ef436bb26901b6fd787445.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
 * main - Entry point
 *
-* Return: 1 (Error)
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-	fputs(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
